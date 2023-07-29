c5fbd8eca54585a8b63365b6b42de351ab26a1ab
SamiZeinAI <zeins20@mycu.concord.edu>
2023-07-29 16:29:24 -0400

    Base functionality

---
+++ client/script.js
@@ -0,0 +1,48 @@
+
+const adjectives = [
+    "clumsy",
+    "silly",
+    "goofy",
+    "enormous",
+    "tiny",
+    "loud",
+    "quiet",
+    "lazy",
+    "energetic",
+    "hilarious"
+  ];
+  
+  const professions = [
+    "astronaut",
+    "chef",
+    "firefighter",
+    "teacher",
+    "doctor",
+    "programmer",
+    "engineer",
+    "actor",
+    "lawyer",
+    "artist"
+  ];
+  
+  const funnyActivities = [
+    "juggling flamingos",
+    "tap dancing on a tightrope",
+    "singing opera in the library",
+    "eating spaghetti with a straw",
+    "doing a handstand on a moving bicycle",
+    "playing chess with a monkey",
+    "baking cookies in a microwave",
+    "swimming in a pool filled with jelly",
+    "painting a mural with a toothbrush",
+    "trying to catch a fish with a lasso"
+  ];
+  
+
+const randomAdjective = adjectives[Math.floor(Math.random() * adjectives.length)];
+const randomProfession = professions[Math.floor(Math.random() * professions.length)];
+const randomActivity = funnyActivities[Math.floor(Math.random() * funnyActivities.length)];
+
+const joke = `Yo mama is so ${randomAdjective}, when she decided to become a ${randomProfession}, they had her ${randomActivity}.`;
+
+console.log(joke);
